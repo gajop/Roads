5df5350c2aad95039d3d07528073613f9184c922
Gajo Petrovic <gajopetrovic@gmail.com>
2020-04-21 22:21:48 +0900

    remove game_spawn (errors) and change modinfo

--- LuaRules/Gadgets/game_spawn.lua
+++
@@ -1,4 +0,0 @@
--- Wiki: http://springrts.com/wiki/Gamedev:Glossary#springcontent.sdz
-
--- Include base content game start unit spawning gadget
-return include("luagadgets/gadgets/game_spawn.lua")
--- ModInfo.lua
+++ ModInfo.lua
@@ -1,15 +1,11 @@
 local modinfo = {
-	name = "ABC - C(hili UI Framework)",
-	shortname = "C",
-	game = "SpringABC",
-	shortgame = "ABC",
+	name = "Roads",
+	shortname = "Roads",
 	description = "Game with directory structure + major files + Chili UI Framework",
 	url = "http://www.springrts.com/wiki/SpringABC",
-	
 	modtype = 1,
-
 	depend = {
-		"Spring Cursors",
+		"Spring Cursors"
 	}
 }
 
